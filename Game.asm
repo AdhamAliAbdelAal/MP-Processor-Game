07d21311ae299245ead1b4e5546ac4aead0fe907
AdhamAliAbdelAal <adhamali20012015@gmail.com>
2022-01-01 17:25:31 +0200

    Hamdy, don't take this copy!

--- Game.asm
+++ Game.asm
@@ -6,7 +6,8 @@ include src1.inc
 include src2.inc
 include srcval.inc
 include BasRel.inc
-include cute.inc
+include cute.inc     
+
 .model huge
 
 
@@ -122,7 +123,16 @@ checkSrcSize db 50
                
 ;---------------------------------------------------- 
 player db 0          
-;---------------------------------------------------
+;-------------------POWER UPS--------------------
+pow1 db "P1$" 
+pow2 db "P2$"
+pow3 db "P3$"
+pow4 db "P4$"
+pow5 db "P5$"   
+
+OFFSET_P4 dw ?      
+IS_P4 db 1
+;------------------------------------------------
 DRAWREG_PLAYERS MACRO
     INPUTFIELD
     ;player one drawings
@@ -148,12 +158,222 @@ ENDM DRAWREG_PLAYERS
         mov cl,1
         enterins:
         DRAWREG_PLAYERS 
-        readmsg instruction
-        executeInstruction instruction 
-        resetins instruction+2 
-        mov_cursor 394Ah
+        readmsg instruction 
+        checkEQUALITY pow1,instruction+2
+        jmp Checkp2Jump 
+        OkP1: 
+        INPUTFIELD
+        resetins instruction+2
+        readmsg instruction 
         xor player,1
-        jmp enterins  
+        CALL executeInstruction 
+        xor player,1 
+        jmp ENDCHECKS      
+        
+        Checkp2Jump:
+        jnz CheckP2
+        jmp OkP1 
+
+        CheckP2:
+        checkEQUALITY pow2,instruction+2
+        jmp Checkp3Jump
+        OkP2:  
+        INPUTFIELD
+        resetins instruction+2
+        readmsg instruction 
+        CALL executeInstruction 
+        xor player,1  
+        CALL executeInstruction
+        xor player,1
+        jmp ENDCHECKS
+        
+        Checkp3Jump:
+        jnz CheckP3
+        jmp OkP2 
+
+        CheckP3:
+        checkEQUALITY pow3,instruction+2
+        jnz CheckP4 
+        ;hamdy logic
+        jmp ENDCHECKS
+        
+        CheckP4:
+        checkEQUALITY pow4,instruction+2
+        jnz CheckP5 
+        mov IS_P4,0
+        jmp ENDCHECKS
+        
+        CheckP5:
+        checkEQUALITY pow5,instruction+2
+        jnz NORMALEXE 
+        mov al,0
+        mov cx,28
+        REP STOSB
+        jmp ENDCHECKS
+          
+         
+         
+        NORMALEXE: 
+        CALL executeInstruction
+        ;--check for p4
+        cmp IS_P4,0
+        jnz ENDCHECKS
+        mov bx,OFFSET_P4
+        mov ax,[bx]
+        and ax,0FFFEH
+        mov [bx],ax 
+        mov IS_P4,1 
+        
+        
+        ENDCHECKS:
+        resetins instruction+2 
+        xor player,1
+        jmp enterins    
 HLT         
 main endp
-end main
+;----------------EXECUTE INSTRUCTION---------------- 
+
+executeInstruction PROC near 
+    push_all
+    cmp instruction+6,'['
+    jmp test0
+    ok0:
+    CheckDestination instruction+6
+    MOV DI,9 
+    jmp tosource
+    test0:
+    jz mshReg
+    jmp ok0
+    
+    mshReg:
+    cmp instruction+8,']'
+    jmp test1
+    ok1:
+    DirectDestination instruction+7
+    MOV DI,10 
+    jmp tosource
+    test1:
+    jnz mshDirect
+    jmp ok1
+    
+    mshDirect:
+    cmp instruction+9,']'
+    jmp test2
+    ok2:
+    RegIndirect instruction+7
+    mov di,11 
+    jmp tosource
+    test2:
+    jnz mshIndirect
+    jmp ok2
+    
+    mshIndirect:
+    cmp instruction+11,']'
+    jmp test3
+    ok3:
+    BasedRelativeDES instruction+7
+    mov di,13
+    jmp tosource
+    test3:
+    jnz tosource
+    jmp ok3
+            
+    tosource:             
+    mov OFFSET_P4,bx
+    cmp di,9
+    JMP src0
+    oksrc0:
+    DetectSourceMode instruction+9
+    jmp insjmp
+    src0:
+    jnz nextdi0
+    jmp oksrc0
+    
+    nextdi0:
+    cmp di,10
+    JMP src1
+    oksrc1:
+    DetectSourceMode instruction+10
+    jmp insjmp
+    src1:
+    jnz nextdi1
+    jmp oksrc1
+    
+    nextdi1:
+    cmp di,11 
+    JMP src2
+    oksrc2:
+    DetectSourceMode instruction+11
+    jmp insjmp
+    src2:
+    jnz nextdi2
+    jmp oksrc2
+    
+    nextdi2:
+    cmp di,13
+    JMP src3
+    oksrc3:
+    DetectSourceMode instruction+13
+    jmp insjmp
+    src3:
+    jnz insjmp
+    jmp oksrc3
+    
+    insjmp:
+    mov al,checkDesSize
+    mov ah,checkSrcSize 
+    mov_cursor 424Ah 
+    printhexa AX
+    cmp al,ah 
+    jmp instest0
+    insok0:
+    cmp al,0
+    jmp instest1
+    insok1:
+    
+    cmp al,1
+    jmp  instest2
+    insok2:
+    jmp executeins
+    instest2:
+    jnz insok2
+    jmp executeins05 
+    
+    
+    
+    instest1:
+    jnz insok1
+    jmp invalidop
+    
+    instest0:
+    
+    jz insok0
+    add al,ah
+    cmp al,2
+    jmp instest3
+    insok3:
+    cmp al,1
+    jmp instest2
+    instest3:
+    jbe insok3
+    mov_cursor 404Ah 
+    printhexa AX 
+    jmp invalidop
+    
+    executeins:
+    checkInsruction  instruction+2
+    jmp leaveexecute
+    executeins05:
+    checkInsruction05  instruction+2
+    jmp leaveexecute
+    invalidop:     
+    ;mov_cursor 424ch
+    printmsg invalid
+    jmp leaveexecute
+    leaveexecute: 
+    MOV checkDesSize, 100
+    MOV checkSrcSize , 50
+    pop_all        
+    ret
+executeInstruction endp
+end main  
--- test.asm
+++ test.asm
@@ -36,26 +36,67 @@ GO MACRO
         mov ah,0
         mov al,13h
         int 10h 
-ENDM GO 
+ENDM GO       
+push_all macro                  ;; push all
+    push ax
+    push bx
+    push cx
+    push dx
+    push si
+    push di
+    push bp
+endm push_all   
+;;**********  
+pop_all macro                  ;; pop all
+    pop bp
+    pop di
+    pop si
+    pop dx
+    pop cx
+    pop bx 
+    pop ax 
+endm pop_all
+;********************************************
+mov_cursor macro num       ;; Move Cursor       
+     push_all
+mov ah,2 
+mov bx,0
+mov dx,num
+int 10h    
+pop_all
+endm mov_cursor
+
+printmsg macro msg       
+    push_all
+    mov ah,9
+    mov dx,offset msg
+    int 21h     
+    pop_all
+endm printmsg
+
+;--------------READ MESSAGE---------------------
+
+ins db 16,?, 16 dup('$') 
+ad db "adhoma$"
 .CODE
 MAIN PROC FAR
 		   mov ax, @data
 		   mov DS, ax 
 		   GO
-		   mov ax,0600h
-mov bh,07
-mov cx,0
-mov dx,184FH
-int 10h
-		   MOV bl,0AH
-		   MOV AX,50H
-		   DIV bl
-		     
-           printhexa dX
-           printhexa ax
+		   mainloop: 
+		   call readmsg 
+		   jmp mainloop
          
-		
+		   adham:
+		   printmsg ad
 	
-
-	MAIN ENDP
+	MAIN ENDP  
+readmsg proc     
+    push_all
+    mov ah,0ah
+    mov dx,offset ins
+    int 21h         
+    pop_all
+    ret 
+readmsg endp
 END MAIN
