e7868fc0d8e981172e47577a59ef0e36ce9526e1
AdhamAliAbdelAal <adhamali20012015@gmail.com>
2022-01-07 23:41:18 +0200

    serial port

--- Game.asm
+++ Game.asm
@@ -253,8 +253,10 @@ checkDesSize db 100
 checkSrcSize db 50
                
 ;---------------------------------------------------- 
+mainPlayer db 1
 player db 0
 level db 0
+value db ?
 
 pointer dw ?   
 GAME_Main_STR  db   '        ',0ah,0dh
@@ -2197,4 +2199,57 @@ executeInstruction PROC near
     pop_all        
     ret
 executeInstruction endp
+
+;-----------------Initialization-----------------------
+init proc near
+    mov dx,3fbh ; Line Control Register
+    mov al,10000000b ;Set Divisor Latch Access Bit
+    out dx,al ;Out it            
+    mov dx,3f8h
+    mov al,0ch 
+    out dx,al
+    mov dx,3f9h
+    mov al,00h
+    out dx,al
+    mov dx,3fbh
+    mov al,00011011b
+    out dx,al
+    ret
+init endp    
+;--------------------------------------------
+;-------------Receive------------------------
+receive proc near 
+mov dx , 3FDH ; Line Status Register  
+
+CHK:
+in al , dx
+AND al,1
+jz CHK
+
+mov dx , 03F8H
+in al , dx
+mov VALUE , al
+
+ret
+receive endp
+;--------------------------------------------------
+
+;---------------------Send------------------------
+send proc near
+mov dx , 3FDH ; Line Status Register   
+
+CheckSend:
+In al , dx ;Read Line Status
+AND al , 00100000b
+JZ CheckSend
+
+mov dx , 3F8H ; Transmit data register
+mov al,VALUE
+out dx , al
+
+
+
+ret
+send endp
+;------------------------------------------------------
 end main  
